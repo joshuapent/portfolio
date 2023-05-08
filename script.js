6f02d8eabd8253efa26086de24f4df7541667e61
joshuapent <joshdavidpent@gmail.com>
2023-05-07 21:35:10 -0500

    Added js function to show one project at a time

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let aboutMe = document.getElementById('about-me')
+const pacman = document.getElementById('pacman')
+const slackOverFlow = document.getElementById('slackoverflow');
+const onyxGaming = document.getElementById('onyx-gaming')
 
-function change() {
-  aboutMe.innerHTML('<h1>Heyo</h1>')
+document.getElementById('pacman-button').onclick = function() {
+  slackOverFlow.classList.remove('project-card-show')
+  onyxGaming.classList.remove('project-card-show')
+  pacman.classList.add('project-card-show');
 }
-
+document.getElementById('slack-button').onclick = function() {
+  pacman.classList.remove('project-card-show')
+  onyxGaming.classList.remove('project-card-show')
+  slackOverFlow.classList.add('project-card-show');
+}
+document.getElementById('onyx-button').onclick = function() {
+  slackOverFlow.classList.remove('project-card-show')
+  pacman.classList.remove('project-card-show')
+  onyxGaming.classList.add('project-card-show');
+}
