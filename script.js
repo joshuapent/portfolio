379f0945c00f295fa1a0fca44b22ea1f8f726973
joshuapent <joshdavidpent@gmail.com>
2023-05-09 12:35:41 -0500

    Added scroll snapping for better UI experience

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const pacman = document.getElementById('pacman')
 const slackOverFlow = document.getElementById('slackoverflow');
 const onyxGaming = document.getElementById('onyx-gaming')
+const home = document.getElementById('home')
+const aboutMe = document.getElementById('about-me')
+const projects = document.getElementById('projects')
+const contact = document.getElementById('contact')
+
 
 document.getElementById('pacman-button').onclick = function() {
   slackOverFlow.classList.remove('project-card-show')
@@ -17,3 +22,8 @@ document.getElementById('onyx-button').onclick = function() {
   pacman.classList.remove('project-card-show')
   onyxGaming.classList.add('project-card-show');
 }
+
+// addEventListener('scroll', function() {
+//   console.log('yo')
+//   // document.getElementById('contact').scrollTop;
+// })
